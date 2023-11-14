5d1e470624725f612a4824da076537ad03557a7b
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-11-14 16:27:48 -0300

    Added test ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,10 +18,37 @@ const getGoogleAuthUrl = () => {
   return `${rootUrl}?${urlSearchParams.toString()}`;
 };
 
+const getFacebookAuthUrl = () => {
+  const rootUrl = 'https://www.facebook.com/v12.0/dialog/oauth';
+
+  const options = {
+    redirect_uri: import.meta.env.VITE_FACEBOOK_OAUTH_REDIRECT_URL,
+    client_id: import.meta.env.VITE_FACEBOOK_CLIENT_ID,
+    access_type: 'offline',
+    response_type: 'code',
+    scope: ['email'].join(' '),
+  };
+
+  const urlSearchParams = new URLSearchParams(options);
+
+  return `${rootUrl}?${urlSearchParams.toString()}`;
+};
+
 function App() {
   return (
     <>
-      <a href={getGoogleAuthUrl()}>Login</a>
+      <a
+        className='text-red-700'
+        href={getGoogleAuthUrl()}
+      >
+        Login
+      </a>
+      <a
+        className='text-blue-700'
+        href={getFacebookAuthUrl()}
+      >
+        Login
+      </a>
     </>
   );
 }
