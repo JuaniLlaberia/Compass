b7b82a0a99e77683d251844c6c0d5b76cd85ffe3
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-11-15 16:37:51 -0300

    Started with users query system

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,4 @@
+const sub = require('date-fns/sub');
 const User = require('../models/userModel');
 const Matches = require('../models/matchesModel');
 
@@ -30,5 +31,55 @@ exports.getAllMatches = async (req, res) => {
       .json({ status: 'success', count: matches.length, data: matches });
   } catch (err) {
     res.status(404).json({ status: 'failed', message: err.message });
+  }
+};
+
+exports.getUsers = async (req, res) => {
+  const { distance, category, gender, minAge, maxAge } = req.user.filters;
+
+  const query = User.find({
+    $and: [
+      { _id: { $ne: req.user._id } }, //Users that are not auth one
+      { hideUser: false }, //That are public/not hidden
+      { newUser: false }, //That have finish acc creation process
+    ],
+  }).select('-email -__v -newUser -hideUser -membership -birthDate');
+
+  //Users that match the category you are searching
+  if (category) query.find({ category });
+
+  //Users that match the gender
+  if (gender) query.find({ gender });
+
+  //Users that match the age range
+  if (minAge || maxAge) {
+    query.find({
+      $and: [
+        { birthDate: { $gte: sub(new Date(), { years: maxAge }) } },
+        { birthDate: { $lte: sub(new Date(), { years: minAge }) } },
+      ],
+    });
+  }
+
+  //Users in a certain distance raidius from you (How to get user location??)
+  if (distance) {
+    const radius = distance / 6378.1;
+
+    const [lat, lng] = req.body.coordinates;
+
+    query.find({
+      location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+    });
+  }
+
+  //FILTER FOR ALREADY SEEN USERS
+  //PAGINATION SYSTEM
+  //SHUFFLE
+
+  try {
+    const users = await query;
+    res.status(200).json({ status: 'success', data: users });
+  } catch (err) {
+    res.status(404).json({ status: 'failed' });
   }
 };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.use(authController.protect);
 
+router.route('/').get(userController.getUsers);
 router.route('/me').get(userController.getUser);
 router.route('/update').patch(userController.updateUser);
 router.route('/matches').get(userController.getAllMatches);
