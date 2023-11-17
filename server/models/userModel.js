6f3e435006cbe24fc57493958eaec031dde1a186
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-11-17 19:17:57 -0300

    Modified users model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -55,6 +55,10 @@ const userSchema = new mongoose.Schema({
     type: Number,
     default: 10,
   },
+  extraLikes: {
+    type: Number,
+    default: 0,
+  },
   location: {
     type: {
       type: String,
