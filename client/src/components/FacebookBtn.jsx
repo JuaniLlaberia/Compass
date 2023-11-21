1e35e8b0c5a1bb17f805974599752c178df484c9
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-11-21 18:48:09 -0300

    Reusable facebook auth button

---
+++ client/src/components/FacebookBtn.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import { FaFacebook } from 'react-icons/fa';
+import { getFacebookAuthUrl } from '../utils/getFacebookAuthUrl';
+
+const FacebookBtn = () => {
+  return (
+    <Link
+      to={getFacebookAuthUrl()}
+      className='flex justify-center items-center gap-2 bg-light-bg-1 active:bg-light-bg-2 md:hover:bg-light-bg-2 shadow-sm border rounded-lg py-3 my-3 font-semibold cursor-pointer transition-colors'
+    >
+      <FaFacebook
+        size={30}
+        fill='#3b5998'
+      />
+      <span>Log in with Google</span>
+    </Link>
+  );
+};
+
+export default FacebookBtn;
