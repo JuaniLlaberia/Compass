48cd7800c28c1c4224e7d86a9e91c479a649fc6e
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-11-20 23:51:41 -0300

    Created navbar

---
+++ client/src/components/MainNav.jsx
@@ -0,0 +1,44 @@
+import {
+  IoSendSharp,
+  IoNotifications,
+  IoChatbubbleEllipsesSharp,
+  IoPerson,
+} from 'react-icons/io5';
+import NavItem from './NavItem';
+
+const items = [
+  {
+    link: '/app',
+    icon: <IoSendSharp size={25} />,
+  },
+  {
+    link: '/notifications',
+    icon: <IoNotifications size={25} />,
+  },
+  {
+    link: '/chats',
+    icon: <IoChatbubbleEllipsesSharp size={25} />,
+  },
+  {
+    link: '/profile',
+    icon: <IoPerson size={25} />,
+  },
+];
+
+const MainNav = () => {
+  return (
+    <nav>
+      <ul className='w-full h-12 fixed bottom-0 z-50 flex justify-around items-center bg-light-bg-1 dark:bg-dark-bg-1'>
+        {items.map(item => (
+          <NavItem
+            key={item.link}
+            link={item.link}
+            icon={item.icon}
+          />
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default MainNav;
