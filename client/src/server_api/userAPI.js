3cb61342ef96d5896927ee4ca539883ea138329b
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-11-21 18:49:58 -0300

    Updated api function in client

--- client/src/server_api/userAPI.js
+++ client/src/server_api/userAPI.js
@@ -1,16 +1,13 @@
-export const updateUser = async ({ userInfo }) => {
+export const updateUser = async userInfo => {
   try {
+    //Sending data with formData because of image
     const response = await fetch('http://localhost:8000/api/user/update', {
       method: 'PATCH',
       credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userInfo),
+      body: userInfo,
     });
 
     // if(!response.ok)
-
     return await response.json();
   } catch (err) {
     console.log(err);
