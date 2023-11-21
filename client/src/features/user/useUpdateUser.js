84c940d16f983828c6f82a15ed3127855d79cd0c
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-11-21 18:49:34 -0300

    Updated user hook

--- client/src/features/user/useUpdateUser.js
+++ client/src/features/user/useUpdateUser.js
@@ -1,13 +1,16 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { updateUser as updateUserAPI } from '../../server_api/userAPI';
+import { useNavigate } from 'react-router-dom';
 
 export const useUpdateUser = () => {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
   const { mutate: updateUser, isLoading: isUpdating } = useMutation({
-    mutationFn: userInfo => updateUserAPI({ userInfo }),
+    mutationFn: userInfo => updateUserAPI(userInfo),
     onSuccess: data => {
       queryClient.setQueryData('auth-user', data?.data);
+      navigate('/app');
     },
     onError: () => {},
   });
