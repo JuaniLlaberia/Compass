cfb1c36b255830003ac3d1d78ff5ee3fef225a2c
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-11-21 18:50:13 -0300

    Started auth page sketch

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,10 +1,18 @@
-import { getGoogleAuthUrl } from '../utils/getGoogleAuthUrl';
+import GoogleBtn from '../components/GoogleBtn';
+import FacebookBtn from '../components/FacebookBtn';
 
 const AuthPage = () => {
   return (
-    <div className='text-black'>
-      <a href={getGoogleAuthUrl()}>Login</a>
-    </div>
+    <main className='text-black p-4 flex flex-col justify-between h-[100dvh]'>
+      <h1 className='text-lg font-semibold text-center mb-5'>LOGO</h1>
+      <h2 className='text-2xl font-bold text-center py-24'>
+        Find your ideal job
+      </h2>
+      <div className='py-8'>
+        <FacebookBtn />
+        <GoogleBtn />
+      </div>
+    </main>
   );
 };
 
