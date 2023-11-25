ccb02182d4e83fd9f190620bb7c8a6ae9dc60f9d
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-11-25 18:55:53 -0300

    Created reusable textarea component

---
+++ client/src/components/TextArea.jsx
@@ -0,0 +1,13 @@
+const TextArea = ({ error, register, ...props }) => {
+  return (
+    <textarea
+      className={`peer bg-transparent min-h-[150px] max-h-48 w-full rounded-lg p-2.5 text-light-text-1 placeholder-transparent focus:outline-none border ${
+        error ? 'border-red-500' : ''
+      } focus:border-secondary-1 md:hover:bg-light-bg-2`}
+      {...register}
+      {...props}
+    />
+  );
+};
+
+export default TextArea;
