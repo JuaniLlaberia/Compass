bbf2307126361528581df2f9a796907b85796d42
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-11-28 12:26:21 -0300

    Added rate limit to server

--- server/routes/swipesRoute.js
+++ server/routes/swipesRoute.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const authController = require('../controllers/authController');
+
 const swipesController = require('../controllers/swipesController');
+
+//See if we leave it or not
+const swipeLimiter = rateLimit({
+  max: 1,
+  windowMs: 800,
+  message: 'Too many swipes registered.',
+});
 
 const router = express.Router();
 
 router.use(authController.protect);
 
-router.route('/left').post(swipesController.swipeLeft);
-router.route('/right').post(swipesController.swipeRight);
+router.route('/left').post(swipeLimiter, swipesController.swipeLeft);
+router.route('/right').post(swipeLimiter, swipesController.swipeRight);
 router.route('/undo').post(swipesController.undoPreviousSwipe);
 
 module.exports = router;
