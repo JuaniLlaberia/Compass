589688605425a20b32773add6338c45d8cdcf39d
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-11-28 15:07:15 -0300

    Built blueprint for auth page

--- client/src/pages/AuthPage.jsx
+++ client/src/pages/AuthPage.jsx
@@ -1,5 +1,6 @@
 import GoogleBtn from '../components/GoogleBtn';
 import FacebookBtn from '../components/FacebookBtn';
+import Modal from '../components/Modal';
 
 const AuthPage = () => {
   return (
@@ -8,10 +9,40 @@ const AuthPage = () => {
       <h2 className='text-2xl font-bold text-center py-24'>
         Find your ideal job
       </h2>
-      <div className='py-8'>
+      <Modal>
+        <div className='py-8'>
+          <Modal.Open opens='login-modal'>
+            <button className='bg-secondary-1 py-2.5 px-6 rounded-full w-full mb-3  text-dark-text-1 font-semibold text-lg'>
+              Login
+            </button>
+          </Modal.Open>
+          <Modal.Open opens='singup-modal'>
+            <button className='bg-secondary-1 py-2.5 px-6 rounded-full w-full text-dark-text-1 font-semibold text-lg'>
+              Get started
+            </button>
+          </Modal.Open>
+        </div>
+        <Modal.Window windowName='login-modal'>
+          <>
+            <h1 className='text-center mb-4'>LOGO</h1>
+            <h2 className='text-lg text-center font-semibold mb-2'>
+              Get started
+            </h2>
+            <p className='px-2 text-light-text-2'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry.
+            </p>
+            <div className='mt-12'>
+              <FacebookBtn />
+              <GoogleBtn />
+            </div>
+          </>
+        </Modal.Window>
+      </Modal>
+      {/* <div className='pb-4 pt-8'>
         <FacebookBtn />
         <GoogleBtn />
-      </div>
+      </div> */}
     </main>
   );
 };
