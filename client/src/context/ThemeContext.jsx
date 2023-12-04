dfe132064ceb3ebe96dfa5dff08cf06e928ba550
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-12-04 15:45:42 -0300

    Finished theme

--- client/src/context/ThemeContext.jsx
+++ client/src/context/ThemeContext.jsx
@@ -1,10 +1,12 @@
 import { createContext, useContext, useState } from 'react';
+const root = document.getElementById('html');
 
 const ThemeContext = createContext();
 
 export const ThemeContextProvider = ({ children }) => {
   const [theme, setTheme] = useState(() => {
     const savedTheme = localStorage.getItem('THEME_SWIPES');
+    root.className = savedTheme.toLowerCase();
     return savedTheme ? savedTheme : 'Light';
   });
 
@@ -20,6 +22,7 @@ export const ThemeContextProvider = ({ children }) => {
 
     setTheme(crr => {
       localStorage.setItem('THEME_SWIPES', crr);
+      root.className = crr.toLowerCase();
       return crr;
     });
   };
