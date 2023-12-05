673bb527b2c41dc08875fe525e9460e93c24e50f
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-12-05 15:19:52 -0300

    Updated empty cards ui

--- client/src/features/user/CardsEmpty.jsx
+++ client/src/features/user/CardsEmpty.jsx
@@ -1,9 +1,13 @@
+import { IoSearchSharp } from 'react-icons/io5';
 import { CardsLink } from '../../components/CardsLink';
 
 const CardsEmpty = () => {
   return (
     <section className='flex flex-col items-center justify-center px-5 h-full'>
-      <h1 className='font-semibold text-light-text-1 dark:text-dark-text-1 mb-3'>
+      <h1 className='flex items-center gap-2 font-semibold text-light-text-1 dark:text-dark-text-1 mb-3'>
+        <span className='text-secondary-1'>
+          <IoSearchSharp size={25} />{' '}
+        </span>
         You have seen them all
       </h1>
       <p className='text-sm text-light-text-2 dark:text-dark-text-2 mb-8'>
