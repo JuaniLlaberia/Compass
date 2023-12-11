3bd2cdaf549356ab624181350737818dfce1fd12
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-12-11 12:08:14 -0300

    Created new context

---
+++ client/src/context/ChatsContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useContext, useState } from 'react';
+
+const ChatContext = createContext();
+
+export const ChatProvider = ({ children }) => {
+  const [recipientUser, setRecipientUser] = useState({});
+  const [isChatActive, setIsChatActive] = useState(false);
+
+  return (
+    <ChatContext.Provider
+      value={{ recipientUser, setRecipientUser, isChatActive, setIsChatActive }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const useChatContext = () => useContext(ChatContext);
