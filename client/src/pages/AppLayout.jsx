2ed655c10b8235f77c29d416c1a0da9c225477c7
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-12-12 15:01:37 -0300

    Updated app layout style

--- client/src/pages/AppLayout.jsx
+++ client/src/pages/AppLayout.jsx
@@ -4,7 +4,7 @@ import MainNav from '../components/MainNav';
 const AppLayout = () => {
   return (
     <>
-      <main className='bg-light-bg-1 dark:bg-dark-bg-1 h-[100dvh] pb-16'>
+      <main className='bg-light-bg-1 dark:bg-dark-bg-1 h-screen pb-16'>
         <Outlet />
       </main>
       <MainNav />
