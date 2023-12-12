e21efa37ff624630d9d4305dab6e64018149c5a3
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-12-12 15:01:29 -0300

    Implemented new profile styling

--- client/src/features/user/ProfileHome.jsx
+++ client/src/features/user/ProfileHome.jsx
@@ -1,55 +1,50 @@
-import { IoImage } from 'react-icons/io5';
-import ProfileBtns from './ProfileBtns';
-import Modal from '../../components/Modal';
-import UpdateImgModal from './UpdateImgModal';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+import { IoClose } from 'react-icons/io5';
+import UserPreview from './UserPreview';
+import ProfileHeader from './ProfileHeader';
 import { useAuthContext } from '../../context/AuthContext';
 
 const ProfileHome = () => {
   const { user } = useAuthContext();
-  const { fullName, category, profileImage } = user.data;
+  const [previewOpen, isPreviewOpen] = useState(false);
 
   return (
-    <>
-      <header>
-        <h1 className='font-semibold text-lg p-4 text-light-text-1 dark:text-dark-text-1'>
-          My Account
-        </h1>
+    <section className='flex h-full w-full'>
+      <header className='md:hidden'>
+        <ProfileHeader userData={user.data} />
+        <button
+          className='absolute top-4 right-5 bg-secondary-1 text-dark-text-1 font-semibold py-1 px-3 rounded-xl'
+          onClick={() => isPreviewOpen(true)}
+        >
+          Preview
+        </button>
       </header>
-      <section className='flex flex-col justify-center items-center mt-2'>
-        <div className='relative'>
-          <img
-            src={profileImage}
-            className='h-32 w-32 xl:h-44 xl:w-44 rounded-full bg-light-bg-3 dark:bg-dark-bg-3'
-          />
-          <Modal>
-            <Modal.Open opens='update-image'>
-              <button className='absolute -bottom-1 -right-1 text-light-text-1 dark:text-dark-text-1 bg-light-bg-1 dark:bg-dark-bg-1 rounded-full p-1.5 border border-light-border-1  dark:border-dark-border-1'>
-                <IoImage size={25} />
-              </button>
-            </Modal.Open>
-            <Modal.Window windowName='update-image'>
-              <UpdateImgModal />
-            </Modal.Window>
-          </Modal>
-        </div>
-        <h1 className='text-lg text-light-text-1 dark:text-dark-text-1 font-semibold xl:text-2xl mt-3'>
-          {fullName}
-        </h1>
-        <ul className='flex justify-center gap-2 flex-wrap px-6 mt-2'>
-          {category.map(cat => (
-            <li
-              className='text-secondary-1 font-semibold border border-light-border-1 dark:border-dark-border-1 rounded-full text-sm px-3 py-0.5 md:text-base xl:text-lg xl:px-5'
-              key={cat}
-            >
-              {cat}
-            </li>
-          ))}
-        </ul>
-      </section>
-      <section className='flex justify-start mt-10'>
-        <ProfileBtns />
-      </section>
-    </>
+      {previewOpen && (
+        <motion.section
+          initial={{ y: '100%' }}
+          animate={{ y: '0%' }}
+          transition={{ duration: 0.25 }}
+          className='fixed w-full h-full bg-light-bg-1 overflow-y-auto pb-24 md:hidden z-[100]'
+        >
+          <button
+            className='absolute top-2.5 right-2.5'
+            onClick={() => isPreviewOpen(false)}
+          >
+            <IoClose size={26} />
+          </button>
+          <UserPreview userData={user.data} />
+        </motion.section>
+      )}
+      <div className='hidden md:flex md:w-full'>
+        <aside className='border-r'>
+          <ProfileHeader userData={user.data} />
+        </aside>
+        <section className='flex justify-center w-full h-full overflow-y-auto'>
+          <UserPreview userData={user.data} />
+        </section>
+      </div>
+    </section>
   );
 };
 
