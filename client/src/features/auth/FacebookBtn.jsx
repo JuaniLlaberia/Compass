5da00fcf350b6864cbd10a0742e309c47a5cc930
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-12-13 15:49:37 -0300

    Fix text error

--- client/src/features/auth/FacebookBtn.jsx
+++ client/src/features/auth/FacebookBtn.jsx
@@ -8,11 +8,8 @@ const FacebookBtn = () => {
       to={getFacebookAuthUrl()}
       className='flex justify-center items-center gap-2 bg-light-bg-1 active:bg-light-bg-2 md:hover:bg-light-bg-2 shadow-sm border rounded-lg py-3 my-3 lg:py-5 font-semibold cursor-pointer transition-colors'
     >
-      <FaFacebook
-        size={30}
-        fill='#3b5998'
-      />
-      <span className='lg:text-xl'>Continue with Google</span>
+      <FaFacebook size={30} fill='#3b5998' />
+      <span className='lg:text-xl'>Continue with Facebook</span>
     </Link>
   );
 };
