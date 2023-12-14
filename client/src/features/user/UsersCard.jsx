c7103c3af88bdb39e663965eb6ef0e8712ec1c90
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-12-14 10:26:38 -0300

    Refactor component

--- client/src/features/user/UsersCard.jsx
+++ client/src/features/user/UsersCard.jsx
@@ -8,7 +8,13 @@ const UsersCard = ({ userData, categories, matchedCategories }) => {
 
   return (
     <>
-      <section>
+      <section
+        className={`${
+          open
+            ? 'px-10 fixed h-screen w-full bg-light-bg-1 dark:bg-dark-bg-1 z-[100] overflow-y-auto pb-4'
+            : ''
+        }`}
+      >
         <UserCardHeader
           userInfo={userData}
           categories={categories}
@@ -16,41 +22,34 @@ const UsersCard = ({ userData, categories, matchedCategories }) => {
           isOpen={open}
           toggleOpen={() => setOpen(true)}
         />
-      </section>
-
-      {open && (
-        <section className='px-10 fixed h-screen w-full bg-light-bg-1 dark:bg-dark-bg-1 z-[100] overflow-y-auto pb-4'>
-          <button
-            onClick={() => setOpen(false)}
-            className='absolute top-2.5 right-2.5 text-2xl lg:text-3xl md:hover:scale-110 transition-all text-light-text-1 dark:text-dark-text-1'
-          >
-            <IoClose />
-          </button>
-          <UserCardHeader
-            userInfo={userData}
-            categories={categories}
-            matchedCategories={matchedCategories}
-            isOpen={open}
-          />
-          <div className='flex flex-col items-center'>
-            <div className='max-w-[18rem] 2xl:max-w-[30rem] w-full gap-2 mt-5 p-2 px-3 border border-light-border-1 dark:border-dark-border-1 rounded-xl shadow-md'>
-              <h3 className='font-semibold text-light-text-2 2xl:text-xl'>
-                About
-              </h3>
-              <p className='text-light-text-1 dark:text-dark-text-1 px-1 2xl:text-lg'>
-                {userData.summary}
-              </p>
-            </div>
-            <Link
-              to='/app/report'
-              className='flex items-center gap-1 py-2 px-5 font-semibold mt-4 border border-light-border-1 dark:border-dark-border-1 rounded-xl bg-dark-bg-2 text-dark-text-1 2xl:gap-2 2xl:text-xl 2xl:mt-8 2xl:py-3 2xl:px-10 active:bg-dark-bg-3'
+        {open && (
+          <>
+            <button
+              onClick={() => setOpen(false)}
+              className='absolute top-2.5 right-2.5 text-2xl lg:text-3xl md:hover:scale-110 transition-all text-light-text-1 dark:text-dark-text-1'
             >
-              <IoFlagOutline size={20} />
-              Report
-            </Link>
-          </div>
-        </section>
-      )}
+              <IoClose />
+            </button>
+            <div className='flex flex-col items-center'>
+              <div className='max-w-[18rem] 2xl:max-w-[30rem] w-full gap-2 mt-5 p-2 px-3 border border-light-border-1 dark:border-dark-border-1 rounded-xl shadow-md'>
+                <h3 className='font-semibold text-light-text-2 2xl:text-xl'>
+                  About
+                </h3>
+                <p className='text-light-text-1 dark:text-dark-text-1 px-1 2xl:text-lg'>
+                  {userData.summary}
+                </p>
+              </div>
+              <Link
+                to='/app/report'
+                className='flex items-center gap-1 py-2 px-5 font-semibold mt-4 border border-light-border-1 dark:border-dark-border-1 rounded-xl bg-dark-bg-2 text-dark-text-1 2xl:gap-2 2xl:text-xl 2xl:mt-8 2xl:py-3 2xl:px-10 active:bg-dark-bg-3'
+              >
+                <IoFlagOutline size={20} />
+                Report
+              </Link>
+            </div>
+          </>
+        )}
+      </section>
     </>
   );
 };
