803ed31cd00fe3ae1a1bd03b222234f286105beb
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-12-14 10:26:29 -0300

    Fixed styling

--- client/src/features/user/HiddenUserCard.jsx
+++ client/src/features/user/HiddenUserCard.jsx
@@ -17,7 +17,7 @@ const HiddenUserCard = () => {
       <button
         aria-label='unhide user'
         onClick={() => updateUser({ hideUser: false })}
-        className='flex justify-center items-center gap-3 min-w-[180px] font-semibold bg-secondary-1 py-2 px-5 rounded-full text-dark-text-1 lg:py-2.5 lg:px-8'
+        className='flex justify-center items-center gap-3 min-w-[180px] font-semibold bg-gradient py-2 px-5 rounded-full text-dark-text-1 lg:py-2.5 lg:px-8'
       >
         {isUpdating ? (
           <ClipLoader
